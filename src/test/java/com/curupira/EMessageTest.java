9ab509e9104adc0611e2768628acb24aee925256
Vitor Mussatto <mussatto@gmail.com>
2015-07-10 16:05:12 -0300

    fixing automated tests, using objectify

--- src/test/java/com/curupira/EMessageTest.java
+++ src/test/java/com/curupira/EMessageTest.java
@@ -8,14 +8,18 @@ import org.junit.Test;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.util.Closeable;
 
 public class EMessageTest {
 	
 	private final LocalServiceTestHelper helper =
 		      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
-	
+	Closeable session;
 	@Before
 	public void setUp() {
+		session = ObjectifyService.begin();
+		ObjectifyService.register(EMessage.class);
 		helper.setUp();
 	}
 	
@@ -31,13 +35,29 @@ public class EMessageTest {
 		String content="Is this the real life? Is this just a fantasy?";
 		String language="en";
 		Date lastUpdated=new Date();
-		EMessage prayer = new EMessage(id, title, content, language, lastUpdated);
+		EMessage message = new EMessage(id, title, content, language, lastUpdated);
 		
-		prayer.persist();
+		message.persist();
 		
-		System.out.println("id generated:"+prayer.getId());
+		System.out.println("id generated:"+message.getId());
 		
+		EMessage messagefound = EMessage.find(message.getId());
+		System.out.println("Message sent:"+message.getContent());
+		System.out.println("Message found:"+messagefound.getContent());
 		
+	}
+	
+	@Test
+	public void testEncoding(){
+		String text = "i am going to encode this text";
+		String pass = "password";
+		String encodedText = MessageEncoder.encode(text, pass);
+		System.out.println(encodedText);
+		
+		String decodedText = MessageEncoder.decode(encodedText, pass);
+		
+		System.out.println(decodedText);
+				
 	}
 
 }
