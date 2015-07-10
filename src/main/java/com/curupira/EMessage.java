05a4119d303ecd8572a3ea19542035bab4cf48ca
Vitor Mussatto <mussatto@gmail.com>
2015-07-10 16:04:09 -0300

    changing to objectify services

--- src/main/java/com/curupira/EMessage.java
+++ src/main/java/com/curupira/EMessage.java
@@ -1,40 +1,38 @@
 package com.curupira;
 
 import java.util.Date;
+import java.util.UUID;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
 @Entity
 public class EMessage {
 	
-	public static final String EMESSAGE_ENTITY_NAME="EMESSAGE";
-	
 	public EMessage(String id, String title, String content, String language, Date lastUpdated) {
 		super();
 		this.title = title;
 		this.content = content;
+		this.language = language;
 		this.lastUpdated = lastUpdated;
+		this.pass = UUID.randomUUID().toString();
+		encodeContent(content);
 	}
 	
 	@Id
-	private String id;
+	private Long id;
 
 	private String title;
 	
 	private String content;
-	
+		
 	private String language;
 	
 	private Date lastUpdated;
+	
+	private String pass;
 	
 
 	public String getTitle() {
@@ -49,11 +47,11 @@ public class EMessage {
 		return lastUpdated;
 	}
 
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 	
-	public void setId(String id){
+	public void setId(Long id){
 		this.id=id;
 	}
 
@@ -66,29 +64,26 @@ public class EMessage {
 	}
 	
 	public void persist(){
-		com.google.appengine.api.datastore.Entity entity;
-		if(this.getId()!=null){
-			Key prayerKey = KeyFactory.createKey(EMessage.EMESSAGE_ENTITY_NAME, id);
-			entity = new com.google.appengine.api.datastore.Entity(EMessage.EMESSAGE_ENTITY_NAME, prayerKey);
-		}else{
-			entity = new com.google.appengine.api.datastore.Entity(EMessage.EMESSAGE_ENTITY_NAME);
-		    entity.setProperty("content", this.getContent());
-		    entity.setProperty("title", this.getTitle());
-		    entity.setProperty("language", this.getLanguage());
-		    entity.setProperty("lastUpdated", this.getLastUpdated());
-		    
-		    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		    Key persistedKey = datastore.put(entity);
-		    this.id = persistedKey.getAppId();
-		}
+	    ObjectifyService.ofy().save().entity(this).now();
 	}
 	
-	public static EMessage find(String key){
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Filter filter = new FilterPredicate(com.google.appengine.api.datastore.Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, key);
-		Query q = new Query(EMESSAGE_ENTITY_NAME).setFilter(filter);
-		return null;
+	
+	public void encodeContent(String pass){
+		this.content = MessageEncoder.encode(content, pass);
 	}
+
+	public String getPass() {
+		return pass;
+	}
+	
+	public static EMessage find(long id){
+		Objectify ofy = ObjectifyService.ofy();
+		ofy = ObjectifyService.ofy();
+		return ofy.load().type(EMessage.class).id(id).now();
+	}
+
+	
+	
 	
 
 }
