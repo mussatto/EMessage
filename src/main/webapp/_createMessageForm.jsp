c13a1b5993051c56eebaa12f3693b61cc904d75e
Vitor Mussatto <mussatto@gmail.com>
2015-08-01 22:58:44 -0300

    possibility to inform password

--- src/main/webapp/_createMessageForm.jsp
+++ src/main/webapp/_createMessageForm.jsp
@@ -5,6 +5,10 @@
     	<input name="title" id="title" class="form-control"/>
    	</div>
    	<div class="form-group">
+    	<label for="passwordCreate">Password</label>
+    	<input name="passwordCreate" id="passwordCreate" class="form-control"/>
+   	</div>
+   	<div class="form-group">
    		<label for="content">Content</label>
    		<textarea name="content" id="content" class="form-control" >
    		</textarea>
--- src/main/webapp/js/createMessage.js
+++ src/main/webapp/js/createMessage.js
@@ -1,21 +1,23 @@
 function submitCreateMessage(){
 	var title = $("#title").val();
 	var content = $("#content").val();
+	var passwordCreate = $("#passwordCreate").val();
 	
 	$.ajax({
 		  url: "/createMessage",
 		  data: {
 		    title: title,
-		    content: content
+		    content: content,
+		    pass: passwordCreate
 		  },
 		  success: function( data ) {
 			var parsed = JSON.parse(data);
 		    $( "#response" ).html( "<div class='alert alert-success'>"
-		    		+"<h2> Message ID:</h2>"
+		    		+"<h2>ID:</h2>"
 		    		+"<p>"+ parsed.id + "</p>"
-		    		+"<h2>Message Pass:</h2><p>"
+		    		+"<h2>Pass:</h2><p>"
 		    		+ parsed.pass+"</p>"
-		    		+"<h2>Message Encoded</h2>"
+		    		+"<h2>Encoded</h2>"
 		    		+"<p>"+parsed.encodedMessage +"<p>"
 		    		+"</div>" );
 		  }
